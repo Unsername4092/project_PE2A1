ed23c6211ad59ff5def1668f0c9de8f47f4ec3ef
leeseonjin01 <lsjlsh1130@gmail.com>
2022-05-19 11:14:50 +0900

    upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PE2_team A1 project
+***
+## INDEX
+* introduction
+* install list
+* usage
+* program
+* contact
+***
+## Introduction
+This program is designed to take inforamtion from users and analyze it. 
+***
+## Install list
+***
+## Usage
+***
+## Program
+***
+## Contact
+
 소개, 목차, 설치할 것, 사용 방법, file 정보, 마무리
--- src/savefig.py
+++ src/savefig.py
@@ -1,10 +1,16 @@
 import os
 
+import matplotlib.pyplot as plt
+
 
 def save_fig(fig, filename):
     fig.suptitle(filename, fontsize=13, fontweight='bold')
     fig.set_size_inches((25, 17), forward=False)
-    fig.savefig('{}/res/{}.png'.format(os.getcwd(), filename))
-
-
-
+    savefig = input('그래프 저장하시겠습니까? (TRUE =1 or FALSE =0) : ')
+    if savefig == 1:
+        fig.savefig('{}/res/{}.png'.format(os.getcwd(), filename))
+    showfig = input('그래프 출력하시겠습니까? (TRUE =1 or FALSE =0) : ')
+    if showfig == 1:
+        plt.show(fig)
+    elif showfig ==0:
+        plt.close(fig)
