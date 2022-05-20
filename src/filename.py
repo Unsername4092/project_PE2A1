9136022bfda48d8aae928363e6f4f52c350988ba
leeseonjin01 <lsjlsh1130@gmail.com>
2022-05-21 00:11:33 +0900

    upload

--- src/filename.py
+++ src/filename.py
@@ -8,32 +8,33 @@ from src.makefolder import *
 import glob2
 import os
 import xml.etree.ElementTree as etree
+import tqdm
 
 def filename(filenamelist):
     file_name = []
-    path = './dat/**/*LMZ*.xml'
+    file_path = []
+    file_basename = ''
+    path = '../dat/**/*LMZ*.xml'
     file_name_list = glob2.glob(path)
-    filepath=[]
     for f in range(len(file_name_list)):
-        file_basename = os.path.basename(file_name_list[f])
-        file_name.append(file_basename)
-        filesplit = file_basename.split('_')
-        filefind=[]
-        for fsp in range(len(filesplit)):
+        file_basename = os.path.basename(file_name_list[f])   # file_basename: HY202103_D07_(-1,-1)_LION1_DCM_LMZC.xml
+        file_name.append(file_basename)                       # file_name: file_basename list
+        file_split = file_basename.split('_')                 # file_split: HY202103, D07, (-1,-1), LION1, DCM, LMZC.xml
+        file_find = []                                        # file_find: find correct file name list
+        for fsp in range(len(file_split)):
             if filenamelist[fsp] == []:
-                filefind.append(filesplit[fsp])
+                file_find.append(file_split[fsp])
             else:
                 for nl in range(len(filenamelist[fsp])):
-                    if filenamelist[fsp][nl] in filesplit[fsp]:
-                        filefind.append(filesplit[fsp])
-        if len(filefind)==6:
-            filepath.append(file_name_list[f])
-    filenamebase=[]
+                    if filenamelist[fsp][nl] in file_split[fsp]:
+                        file_find.append(file_split[fsp])
+        if len(file_find) == 6:
+            file_path.append(file_name_list[f])
+    filenamebase = []
     for j in range(len(file_name)):
-        base_name = os.path.basename(file_name[j])
-        filenamebase.append(base_name.replace('.xml',''))
+        filenamebase.append(file_basename.replace('.xml', ''))  # file_namebase: HY202103_D07_(-1,-1)_LION1_DCM_LMZC
     root = []
-    for k in filepath:
+    for k in file_path:
         xml = etree.parse(k)
         root.append(xml.getroot())
     return root, filenamebase
